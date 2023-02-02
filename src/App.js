488db6dca3b0d4ecba73ea70a6d28d9a2d3d3cea
Oliver Samuels <osamuels@bu.edu>
2023-02-02 16:09:43 -0500

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-// import logo from './logo.svg';
+
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './App.css';
 import Artist from './components/Artist.js';
 import data1 from './artistData.js';
 import data2 from './albumData.js';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router';
+// import { BrowserRouter, Route, Switch } from 'react-router';
 function App() {
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'a7794b9a36mshed157cdb4537f8ap1b3af5jsn3878cc4f1a3c',
-      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-    }
-  };
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': 'a7794b9a36mshed157cdb4537f8ap1b3af5jsn3878cc4f1a3c',
+  //     'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+  //   }
+  // };
   const artistData = data1.map((artist, index) => {
-
     return <Artist
       key = {index}
       artistName = {artist.artistName}
@@ -28,6 +30,18 @@ function App() {
       artistImg = {album.albumImg}
     />
   })
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+        setCurrentIndex(currentIndex => (currentIndex + 1) % artistData.length);
+        // setCurrentIndex(currentIndex => (currentIndex === artistData.length - 1) ? 0 : currentIndex + 1);
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
+  // setCurrentIndex(currentIndex => (currentIndex === artistData.length - 1) ? 0 : currentIndex + 1);
+
+
+
   // const data = React.useEffect(() => {
   // fetch('https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
   //   .then(response => response.json())
@@ -36,7 +50,41 @@ function App() {
   // })
   // console.log("data", data);
 
-
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 10,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ],
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+  };
   
   return (
     <div className="App">
@@ -47,14 +95,18 @@ function App() {
       </div>
       
       <div className = "artistComponents">
-        {artistData}
+        <Slider className = "slider" {...settings}>
+          {artistData}
+        </Slider>
       </div>
       
       <div className = "artistTitle"> Get the latest releases from your favorite artists
       </div>
 
       <div className = "artistComponents">
-        {albumData}        
+        <Slider className = "slider" {...settings}>
+          {albumData}        
+        </Slider>
       </div>
 
       <div className='downloadInfo'>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,16 +3,21 @@ import "./css/signup.css";
 
 export default function Signup(){
     return (
-        <div className="Signup">
+        <div className="signup">
             <h1 className = "signText"> Signup Now </h1>
             <div className = "box">
                 <form className = "form">
-                    <input type = "text" name = "firstName"></input>
-                    <input type = "text" name = "lastName" ></input>
-                    <input type = "text" name = "email" ></input>
-                    <input type = "text" name = "password" ></input>
-                    <input type = "text" name = "confirmPassword" ></input> 
-                    <input type = "submit" value = "Submit"></input>      
+                    <label for = "firstName"><b>First Name</b></label>
+                    <input type = "text" name = "firstName" placeholder="First Name"></input>
+                    <label for = "lastName"><b>Last Name</b></label>
+                    <input type = "text" name = "lastName" placeholder="Last Name"></input>
+                    <label for = "email"><b>Email</b></label>
+                    <input type = "text" name = "email" placeholder="Enter Email" ></input>
+                    <label for = "password"><b>Password</b></label>
+                    <input type = "text" name = "password" placeholder="Enter Password" ></input>
+                    <label for = "confirmPassword"><b>Confirm Password</b></label>
+                    <input type = "text" name = "confirmPassword" placeholder="Confirm Password"></input> 
+                    <input type = "submit" value = "Submit"  href = "/userdashboard"></input>      
                 </form>
             </div>
         </div>
